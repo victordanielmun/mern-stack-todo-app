d1221b53b9b1ae4ea0dcaa5d7a0ab981fc289a54
victordanielmun <victordanielmun@gmail.com>
2023-03-30 22:37:40 -0500

    se añade archivo .env variable de entorno proxy localhost

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import TaskList from "./components/TaskList";
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+
+export const URL = process.env.REACT_APP_SERVER_URL
+
 
 function App() {
   return (
