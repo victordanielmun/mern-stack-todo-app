face0698b9ef4f670f55a1f8d88a06e7d3cd3d0f
victordanielmun <victordanielmun@gmail.com>
2023-03-31 20:03:39 -0500

    se añade funcion obtener tareas, borrar tarea, seleccionar tarea para editar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,14 @@
 import TaskList from "./components/TaskList";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-export const URL = process.env.REACT_APP_SERVER_URL
-
+export const URL = process.env.REACT_APP_SERVER_URL;
 
 function App() {
   return (
     <div className="app">
       <div className="task-container">
-        <TaskList/>
+        <TaskList />
       </div>
       <ToastContainer />
     </div>
